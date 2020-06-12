e249bc490f39cec7bbccee32b571bab9cd844332
leista <junl@nait.ca>
2020-06-12 11:21:25 -0600

    commit

---
+++ #month.py
@@ -0,0 +1,6 @@
+months="JanFebMarAprMayJunJulAugSepOctNovDec"
+n=input("Number: ")
+pos=(int(n)-1)*3
+monthAbbrev=months[pos:pos+3]
+print(monthAbbrev)
+print("hello world")
---
+++ #week.py
@@ -0,0 +1,5 @@
+months="MonTueWedThuFriSatSun"
+n=input("Number: ")
+pos=(int(n)-1)*3
+dayAbbrev=months[pos:pos+3]
+print(dayAbbrev) 
